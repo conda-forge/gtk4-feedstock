3aceb12e011b05c2fb13dac0a013fddb8a543e0f
Dan Yeaw <dan@yeaw.me>
2025-08-30 14:17:20 -0400

    Fix gtk_doc option not found

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,7 +15,7 @@ export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
 export XDG_DATA_DIRS=${XDG_DATA_DIRS}:$PREFIX/share
 
 meson_config_args=(
-    -Dgtk_doc=false
+    -Ddocumentation=false
     -Dman-pages=false
     -Dintrospection=enabled
     -Dbuild-examples=false
