07c207502f8ae43d470c5ec1320bd565e24d7ea6
Felix Häcker <felix@prefix.dev>
2026-02-19 20:53:05 +0100

    Require c_stdlib_version 2.34

--- recipe/build.sh
+++ recipe/build.sh
@@ -49,27 +49,35 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
     export LDFLAGS=${LDFLAGS//$PREFIX/$BUILD_PREFIX}
     export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
 
-    cat <<EOF > $SRC_DIR/native_file.txt
-[binaries]
-ld = '$($CC_FOR_BUILD -print-prog-name=ld)'
-objcopy = '$($CC_FOR_BUILD -print-prog-name=objcopy)'
-EOF
-
     # Unset them as we're ok with builds that are either slow or non-portable
     unset CFLAGS
     unset CPPFLAGS
     export host_alias=$build_alias
 
+    native_extra_args=()
+    if test $(uname) == 'Linux' ; then
+      # On Linux, the x11/wayland libs are only in $PREFIX (host arch), not
+      # $BUILD_PREFIX, so disable those backends and use broadway instead.
+      cat <<EOF > $SRC_DIR/native_file.txt
+[binaries]
+ld = '$($CC_FOR_BUILD -print-prog-name=ld)'
+objcopy = '$($CC_FOR_BUILD -print-prog-name=objcopy)'
+EOF
+      native_extra_args+=(
+        --native-file=$SRC_DIR/native_file.txt
+        -Dx11-backend=false
+        -Dwayland-backend=false
+        -Dbroadway-backend=true
+      )
+    fi
+
     meson setup native-build \
         "${meson_config_args[@]}" \
-        --native-file=$SRC_DIR/native_file.txt \
+        "${native_extra_args[@]}" \
         --buildtype=release \
         --prefix=$BUILD_PREFIX \
         -Dlibdir=lib \
         --wrap-mode=nofallback \
-        -Dx11-backend=false \
-        -Dwayland-backend=false \
-        -Dbroadway-backend=true \
         || (cat native-build/meson-logs/meson-log.txt; false)
 
     # print full meson configuration
